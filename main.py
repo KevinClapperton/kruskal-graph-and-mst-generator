fcd11cea530818d6e861a08b74a2bcbabbcbc712
the_barista <keclapperton@gmail.com>
2023-03-19 15:20:24 -0400

    Separate the Graph Class in two (Graph and Kruskal)

--- main.py
+++ main.py
@@ -5,11 +5,27 @@
 class Graph:
     def __init__(self, vertices):
         self.V = vertices
-        self.graph = []
+        self._graph = []
+        self._edge_index = 0
+        self._number_of_edges = 0
 
-    def add_edge(self, u, v, w):
-        self.graph.append([u, v, w])
+    def add_edge(self, edge):
+        self._graph.append(edge)
+        self._number_of_edges = self._number_of_edges + 1
 
+    def __iter__(self):
+        return self 
+
+    def __next__(self):
+        if self._edge_index < (self._number_of_edges - 1):            
+            self._edge_index += 1
+            return self._graph[self._edge_index]        
+        raise StopIteration
+
+    def __getitem__(self, idx): 
+        return self._graph[idx]
+
+class Kruskal:
     # Search function
 
     def find(self, parent, i):
@@ -29,17 +45,18 @@ class Graph:
             rank[xroot] += 1
 
     #  Applying Kruskal algorithm
-    def kruskal_algo(self):
+    def run(self, graph):
         result = []
         i, e = 0, 0
-        self.graph = sorted(self.graph, key=lambda item: item[2])
+        graphSortedList = sorted(graph, key=lambda item: item[2])
+        graph._graph = graphSortedList
         parent = []
         rank = []
-        for node in range(self.V):
+        for node in range(graph.V):
             parent.append(node)
             rank.append(0)
-        while e < self.V - 1:
-            u, v, w = self.graph[i]
+        while e < graph.V - 1:
+            u, v, w = graph[i]
             i = i + 1
             x = self.find(parent, u)
             y = self.find(parent, v)
@@ -47,24 +64,27 @@ class Graph:
                 e = e + 1
                 result.append([u, v, w])
                 self.apply_union(parent, rank, x, y)
-        for u, v, weight in result:
-            print("%d - %d: %d" % (u, v, weight))
+
+        # Making resulted graph 
+        resultGraph = Graph(graph.V)
+        for edge in range(len(result)): 
+            resultGraph.add_edge(result[edge])
+
+        return resultGraph
 
 
 g = Graph(6)
-g.add_edge(0, 1, 4)
-g.add_edge(0, 2, 4)
-g.add_edge(1, 2, 2)
-g.add_edge(1, 0, 4)
-g.add_edge(2, 0, 4)
-g.add_edge(2, 1, 2)
-g.add_edge(2, 3, 3)
-g.add_edge(2, 5, 2)
-g.add_edge(2, 4, 4)
-g.add_edge(3, 2, 3)
-g.add_edge(3, 4, 3)
-g.add_edge(4, 2, 4)
-g.add_edge(4, 3, 3)
-g.add_edge(5, 2, 2)
-g.add_edge(5, 4, 3)
-g.kruskal_algo()
+
+g.add_edge([0, 1, 4])
+g.add_edge([0, 2, 4])
+g.add_edge([1, 2, 2])
+g.add_edge([2, 3, 3])
+g.add_edge([2, 5, 2])
+g.add_edge([2, 4, 4])
+g.add_edge([3, 4, 3])
+g.add_edge([5, 4, 3])
+
+algo = Kruskal() 
+newG = algo.run(g)
+
+print(newG._graph)
